2acb1e41460f22ae774ecfceddb9fe2ede8bd973
Patrick Long <patrick.long@gmail.com>
2021-12-25 18:21:12 -0800

    setup fasta download and checksums

---
+++ src/retrieve_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -P ./../data/ ftp.ensembl.org/pub/release-105/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.chromosome.1.fa.gz 
+
+wget -P ./../data/ ftp.ensembl.org/pub/release-105/fasta/mus_musculus/dna/CHECKSUMS
